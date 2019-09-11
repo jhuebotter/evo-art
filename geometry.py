dae4e6e14c2e000342d6ebd757ada14f98eb1b99
jhuebotter <hybi_357@live.de>
2019-09-11 21:05:23 +0200

    v0.01

--- geometry.py
+++ geometry.py
@@ -1,10 +1,13 @@
 import pygame
 import math
+import time
 from music import *
+
+# Define some notes for conversion
+C, Cs, D, Ds, E, F, Fs, G, Gs, A, As, B = range(24, 36)
 
 # Initialize the game engine
 from typing import Any
-
 pygame.init()
 
 # Define the colors we will use in RGB format
@@ -18,6 +21,9 @@ RED = (200, 100, 100)
 size = [1440, 900]
 center = [size[0] / 2, size[1] / 2]
 screen = pygame.display.set_mode(size)
+
+# Set the maximum iterations per second
+fps = 100
 stepsize = 0.2
 
 pygame.display.set_caption("Evo Art")
@@ -32,23 +38,21 @@ xy2polar = lambda x,y: (np.sqrt(x**2 + y**2), np.arctan2(y, x))
 def main():
 
     clock = pygame.time.Clock()
-    i = 0.
 
-    genes1 = dict(size=40, order=3, color=GREEN, number=3, factor=2., line=1, speed=3., delta_offset=-0.,
-                  initial_offset=0, center=center, cutoff=70, amp=1., decay=0.05, decay_level=0.0,
+    genes1 = dict(rootnote=C, rootoctave=4, size=40, order=6, color=GREEN, number=3, line=1,
+                  delta_offset=0., bpm=60, total_offset=0.,
+                  initial_offset=0.5, center=center, cutoff=50, amp=0.5, decay=0.05, decay_level=0.0,
                   sustain=0.3, sustain_level=0.5, release=5, detune=0.4, env_curve=7, mod_pulse_width=0.5)
-    genes2 = dict(size=80, order=4, color=RED, number=3, factor=2., line=1, speed=8., delta_offset=-0.,
-                  initial_offset=22.5, center=center, cutoff=70, amp=1., decay=0.05, decay_level=0.0,
+    genes2 = dict(rootnote=A, rootoctave=3, size=40, order=8, color=GREEN, number=2, line=1,
+                  delta_offset=0., bpm=60, total_offset=0.,
+                  initial_offset=0.5, center=center, cutoff=70, amp=0.5, decay=0.05, decay_level=0.0,
                   sustain=0.3, sustain_level=0.5, release=5, detune=0.4, env_curve=7, mod_pulse_width=0.5)
-    '''
-    genes3 = dict(size=10, order=3, color=GREEN, number=5, factor=2., line=1, speed=3., delta_offset=1.,
-                  initial_offset=0., center=center, cutoff=70, amp=1., decay=0.05, decay_level=0.0,
-                  sustain=0.3, sustain_level=0.5, release=5, detune=0.4, env_curve=7, mod_pulse_width=0.5)
-    genes4 = dict(size=10, order=3, color=GREEN, number=5, factor=2., line=1, speed=-3., delta_offset=-1.,
-                  initial_offset=0., center=center, cutoff=70, amp=1., decay=0.05, decay_level=0.0,
-                  sustain=0.3, sustain_level=0.5, release=5, detune=0.4, env_curve=7, mod_pulse_width=0.5)
-    '''
-    genepool = [genes1] #, genes2] #, genes3, genes4] #, genes5]
+
+    genepool = [genes1]
+
+    # get some time info
+    start = time.time()
+    now = time.time()
 
     # Loop until the user clicks the close button.
     done = False
@@ -56,7 +60,14 @@ def main():
 
         # This limits the while loop to a max of 10 times per second.
         # Leave this out and we will use all CPU we can.
-        clock.tick(100)
+        clock.tick(fps)
+
+        # Time each iteration to know how far to move the geometry
+        t_minus1 = now - start
+        now = time.time()
+        t0 = now - start
+        delta_t = t0 - t_minus1
+        # print(delta_t)
 
         for event in pygame.event.get():  # User did something
             if event.type == pygame.QUIT:  # If user clicked close
@@ -64,22 +75,16 @@ def main():
 
         # All drawing code happens after the for loop and but
         # inside the main while done==False loop.
-
         # Clear the screen and set the screen background
         screen.fill(BLACK)
 
-        i += stepsize
-        #if i > 360.:
-        #    i = 0.
-
+        # This is where the magic happens
         for genes in genepool:
-            #genes['current_offset'] = i * genes['speed']
-            make_polygon(genes, i)
+            make_polygon(genes, t0, delta_t)
 
         # This MUST happen after all the other drawing commands.
         pygame.display.flip()
-        # wait 60 ms until loop restart
-        #pygame.time.wait(10)
+
 
 
     # Be IDLE friendly
@@ -89,30 +94,41 @@ def main():
 
 
 def play_sound(genes):
-    genes['note'] = math.log(genes['radius'], 2) * 12
-    print(genes['note'])
+    print()
+    print('Note:  ', genes['note'])
+    print('Radius:', genes['radius'])
     play_piano(genes)
-    pass
+    return
 
 
 def pol2cart(rho, phi, center=center):
+    # get cartesian coordinates from polar
     x = center[1] + rho * math.cos(math.radians(phi))
     y = center[0] - rho * math.sin(math.radians(phi))
+
     return [y, x]
 
 
 def rotatePoint(polarcorner, angle, center=center):
-
+    # the name explains this pretty well i think
     newPolarcorner = [polarcorner[0], polarcorner[1] + angle]
 
     return newPolarcorner
 
 
-def make_polygon(genes, c):
+def make_polygon(genes, t, delta_t):
     for i in range(genes['number']):
-        genes['radius'] = genes['size'] * (genes['factor']**(i))
-        prev_angle = round((genes['delta_offset'] * (c-stepsize) + genes['initial_offset']) * i + genes['speed'] * (c-stepsize), 3)
-        current_angle = round((genes['delta_offset'] * c + genes['initial_offset']) * i + genes['speed'] * c, 3)
+        factor = round(1. / math.cos(math.radians(180./genes['order'])), 3)
+        #print(factor)
+        genes['note'] = genes['rootnote'] + (12 * (i + genes['rootoctave'] - 1) * factor / 2.)
+        genes['radius'] = ((genes['rootnote']) * (factor**((i + genes['rootoctave'] - 1))))
+
+        # get the rotation angles
+        prev_angle = round((t-delta_t) * (360. / genes['order']) * (genes['bpm'] / 60.), 3)
+        current_angle = round(t * (360. / genes['order']) * (genes['bpm'] / 60.), 3)
+        prev_angle += (genes['initial_offset'] * i + genes['total_offset']) * (360. / genes['order'])
+        current_angle += (genes['initial_offset'] * i + genes['total_offset']) * (360. / genes['order'])
+
         pos = []
         for o in range(genes['order']):
             polarcorner = [genes['radius'], o*(360/genes['order'])]
